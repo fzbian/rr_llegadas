395c85ca9e1f418ff7ec40baee68e57a11039b74
Fabian Martin <fabianmartinanz@gmail.com>
2024-11-04 21:56:23 -0500

    feat: mejores

--- server.py
+++ server.py
@@ -10,43 +10,10 @@ load_dotenv()
 
 app = Flask(__name__)
 
-def get_arrival_time(local, fecha):
-    dia_semana = fecha.strftime("%A")
-    if local == "VIS":
-        horarios = {
-            "Monday": "09:00 AM",
-            "Tuesday": "12:00 PM",
-            "Wednesday": "09:00 AM",
-            "Thursday": "12:00 PM",
-            "Friday": "12:00 PM",
-            "Saturday": "09:00 AM",
-            "Sunday": "10:00 AM"
-        }
-    elif local == "LON":
-        horarios = {
-            "Monday": "09:00 AM",
-            "Tuesday": "09:00 AM",
-            "Wednesday": "10:00 AM",
-            "Thursday": "09:00 AM",
-            "Friday": "09:00 AM",
-            "Saturday": "10:00 AM",
-            "Sunday": "10:00 AM"
-        }
-    elif local == "SAJ":
-        horarios = {
-            "Monday": "10:00 AM",
-            "Tuesday": "10:00 AM",
-            "Wednesday": "10:00 AM",
-            "Thursday": "10:00 AM",
-            "Friday": "10:00 AM",
-            "Saturday": "10:00 AM",
-            "Sunday": "11:00 AM"
-        }
-    
-    return datetime.strptime(horarios[dia_semana], "%I:%M %p").time() if dia_semana in horarios else None
-
 def generate_arrival_difference_table(local, fecha_inicio, fecha_fin):
-    engine = create_engine(f'mysql+mysqlconnector://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}')
+    engine = create_engine(f'mysql+mysqlconnector://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}',
+        connect_args={"auth_plugin": "mysql_native_password"}
+    )
     
     query = f"""
     SELECT fecha, hora_llegada
@@ -61,15 +28,11 @@ def generate_arrival_difference_table(local, fecha_inicio, fecha_fin):
     
     df['hora_llegada'] = df['hora_llegada'].dt.components['hours'].astype(str).str.zfill(2) + ':' + \
                          df['hora_llegada'].dt.components['minutes'].astype(str).str.zfill(2)
-    df['hora_llegada'] = pd.to_datetime(df['hora_llegada'], format='%H:%M', errors='coerce').dt.time
+    df['hora_llegada'] = pd.to_datetime(df['hora_llegada'], format='%H:%M', errors='coerce').dt.strftime('%I:%M %p')
     
-    df['Hora Esperada'] = df['fecha'].apply(lambda x: get_arrival_time(local, x))
-    df['Diferencia'] = df.apply(lambda row: 
-        (datetime.combine(row['fecha'], row['hora_llegada']) - 
-         datetime.combine(row['fecha'], row['Hora Esperada'])).total_seconds() / 60 
-        if pd.notna(row['hora_llegada']) and pd.notna(row['Hora Esperada']) else None, axis=1)
+    df['fecha'] = pd.to_datetime(df['fecha']).dt.strftime('%d %B del %Y')
     
-    result_df = df[['fecha', 'hora_llegada', 'Diferencia']]
+    result_df = df[['fecha', 'hora_llegada']]
     return result_df
 
 @app.route("/", methods=['GET', 'POST'])
@@ -95,18 +58,14 @@ def index():
                 if isinstance(result, str):
                     return render_template('index.html', error=result)
                 else:
-                    total_difference = result['Diferencia'].sum()
-                    total_hours = total_difference // 60
-                    total_minutes = total_difference % 60
-
                     table_html = result.to_html(
                         classes='table table-striped table-bordered',
                         index=False,
                         header=True,
                         justify='center'
-                    )
+                    ).replace('<th>fecha</th>', '<th>Fecha</th>').replace('<th>hora_llegada</th>', '<th>Hora llegada</th>')
 
-                    return render_template('index.html', table=table_html, total_hours=total_hours, total_minutes=total_minutes)
+                    return render_template('index.html', table=table_html)
             except mysql.connector.errors.DatabaseError as e:
                 if e.errno == 1525:
                     return render_template('index.html', error="Por favor, ingresa fechas válidas.")
@@ -120,4 +79,4 @@ def index():
         return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
